51be4f2f8138be05d693009a9f5e8301a53beeb2
c0re <c0re@psypanda.org>
2013-02-13 19:48:52 +0100

    new version
    complete rewrite

--- php/hashid.php
+++ php/hashid.php
@@ -2,11 +2,11 @@
 /******************************************************************************
 * @name: hashIdentify.php
 * @author: c0re <https://psypanda.org/>							
-* @date: 2013/01/29
+* @date: 2013/02/13
 * @copyright: <http://creativecommons.org/licenses/by-nc-sa/3.0/>
 * @readme: <http://wiki.insidepro.com/index.php/Algorithms>
 ******************************************************************************/
-$version = "v1.0";
+$version = "v2.0";
 
 ?>
 
@@ -31,142 +31,92 @@ $version = "v1.0";
 
 function IdentifyHash($str)
 {
+  //set the hash array
+  $hashes = array
+  (
+    '/^[a-f0-9]{4}$/i' => 'CRC-16,CRC-16-CCITT,FCS-16',
+    '/^[a-f0-9]{8}$/i' => 'Adler32,CRC-32,CRC-32B,FCS-32,GHash-32-3,GHash-32-5',
+    '/^\+[a-z0-9\/\.]{12}$/i' => 'Blowfish(Eggdrop)',
+    '/^.{0,2}[a-z0-9\/\.]{11}$/i' => 'DES(Unix)',
+    '/^[a-f0-9]{16}$/i' => 'MySQL3.x,LM,DES(Oracle),VNC',
+    '/^[a-z0-9\/\.]{16}$/i' => 'MD5(Cisco PIX)',
+    '/^\$1\$.{0,8}\$[a-z0-9\/\.]{22}$/i' => 'MD5(Unix)',
+    '/^\$apr1\$.{0,8}\$[a-z0-9\/\.]{22}$/i' => 'MD5(APR)',
+    '/^[a-f0-9]{24}$/i' => 'CRC-96(ZIP)',
+    '/^\$H\$[a-z0-9\/\.]{31}$/i' => 'MD5(phpBB3)',
+    '/^\$P\$[a-z0-9\/\.]{31}$/i' => 'MD5(Wordpress)',
+    '/^[0-9a-f]{32}$/i' => 'MD5,NTLM,Domain Cached Credentials,Domain Cached Credentials 2,RAdmin v2.x,MD4,MD2,RIPEMD-128,Haval-128,Tiger-128,Snefru-128,Skein-256(128),Skein-512(128)',
+    '/^0x[a-f0-9]{32}$/i' => 'Lineage II C4',
+    '/^[a-f0-9]{32}:[a-z0-9]{32}$/i' => 'MD5(Joomla)',    
+    '/^[a-f0-9]{32}:.{5}$/i' => 'MD5(IP.Board)',
+    '/^[a-f-0-9]{32}:[a-z0-9]{8}$/i' => 'MD5(MyBB)',
+    '/^[a-f0-9]{40}$/i' => 'SHA-1,MySQL4.x,RIPEMD-160,Haval-160,SHA-1(MaNGOS),SHA-1(MaNGOS2),Tiger-160,Skein-256(160),Skein-512(160)',
+    '/^\*[a-f0-9]{40}$/i' => 'MySQL5.x',
+    '/^sha1\$.{0,32}\$[a-f0-9]{40}$/i' => 'SHA-1(Django)',
+    '/^0x0100[a-f0-9]{0,8}?[a-f0-9]{40}$/i' => 'MSSQL(2005),MSSQL(2008)',
+    '/^[a-f0-9]{48}$/i' => 'Haval-192,Tiger-192',
+    '/^[a-f0-9]{51}$/i' => 'MD5(Palshop)',
+    '/^\$S\$[a-z0-9\/\.]{52}$/i' => 'SHA-512(Drupal)',
+    '/^\$2a\$[0-9]{0,2}?\$[a-z0-9\/\.]{53}$/i' => 'Blowfish(OpenBSD)',
+    '/^[a-f0-9]{56}$/i' => 'SHA-224,Haval-224,Keccak-224,Skein-256(224),Skein-512(224)',
+    '/^[a-f0-9]{64}$/i' => 'SHA-256,RIPEMD-256,Haval-256,Snefru-256,GOST R 34.11-94,Keccak-256,Skein-256,Skein-512(256)',
+    '/^sha256\$.{0,32}\$[a-f0-9]{64}$/i' => 'SHA-256(Django)',
+    '/^\$5\$.{0,22}\$[a-z0-9\/\.]{43,69}$/i' => 'SHA-256(Unix)',
+    '/^[a-f0-9]{80}$/i' => 'RIPEMD-320',
+    '/^0x0100[a-f0-9]{0,8}?[a-f0-9]{80}$/i' => 'MSSQL(2000)',
+    '/^\$6\$.{0,22}\$[a-z0-9\/\.]{86}$/i' => 'SHA-512(Unix)',
+    '/^[a-f0-9]{96}$/i' => 'SHA-384,Keccak-384,Skein-512(384),Skein-1024(384)',
+    '/^sha384\$.{0,32}\$[a-f0-9]{96}$/i' => 'SHA-384(Django)',
+    '/^[a-f0-9]{128}$/i' => 'SHA-512,Whirlpool,Keccak-512,Skein-512,Skein-1024(512)',
+    '/^[a-f0-9]{256}$/i' => 'Skein-1024',
+    '/^({SSHA})?[a-z0-9\+\/]{32,38}?(==)?$/i' => 'SSHA-1',
+    '/^\(?[a-z0-9\+\/]{20}\)?$' => 'Lotus Domino',
+    '/^[a-f0-9]{32}:[a-z0-9]{2}$' => 'MD5(osCommerce)',
+  );
+           
   //initialize the array
   $possibleHashes = array();
-	
-  if(preg_match('/^[a-f0-9]{4}$/i', $str)) {
-    array_push($possibleHashes,'CRC-16','CRC-16-CCITT','FCS-16');
+  $nextHash = array();
+     
+  //loop and find matches
+  foreach($hashes as $key => $value) {
+    if(preg_match($key, $str)) {
+      //explode the hashlist
+      $nextHash = explode(",", $value);
+      foreach($nextHash as $newVal) {
+        //append to array
+        array_push($possibleHashes, $newVal);
+      }
+    }
   }
-  if(preg_match('/^[a-f0-9]{8}$/i', $str)) {
-    array_push($possibleHashes,'Adler32','CRC-32','CRC-32B','FCS-32','GHash-32-3','GHash-32-5');
-  }
-  if(preg_match('/^\+[a-z0-9\/\.]{12}$/i', $str)) {
-    array_push($possibleHashes,'Blowfish(Eggdrop)');
-  }
-  if(preg_match('/^.{0,2}[a-z0-9\/\.]{11}$/i', $str)) {
-    array_push($possibleHashes,'DES(Unix)');
-  }
-  if(preg_match('/^[a-f0-9]{16}$/i', $str)) {
-    array_push($possibleHashes,'MySQL3.x','LM','DES(Oracle)','VNC');
-  }
-  if(preg_match('/^[a-z0-9\/\.]{16}$/i', $str)) {
-    array_push($possibleHashes,'MD5(Cisco PIX)');
-  }
-  if(preg_match('/^\$1\$.{0,8}\$[a-z0-9\/\.]{22}$/i', $str)) {
-    array_push($possibleHashes,'MD5(Unix)');
-  }
-  if(preg_match('/^\$apr1\$.{0,8}\$[a-z0-9\/\.]{22}$/i', $str)) {
-    array_push($possibleHashes,'MD5(APR)');
-  }
-  if(preg_match('/^[a-f0-9]{24}$/i', $str)) {
-    array_push($possibleHashes,'CRC-96(ZIP)');
-  }
-  if(preg_match('/^\$H\$9[a-z0-9\/\.]{30}$/i', $str)) {
-    array_push($possibleHashes,'MD5(phpBB3)');
-  }
-  if(preg_match('/^\$P\$[a-z0-9\/\.]{31}$/i', $str)) {
-    array_push($possibleHashes,'MD5(Wordpress)');
-  }
-  if(preg_match('/^[0-9a-f]{32}$/i', $str)) {
-    array_push($possibleHashes,'MD5','NTLM','Domain Cached Credentials','Domain Cached Credentials 2','MD4','MD2','RIPEMD-128','Haval-128','Tiger-128','Snefru-128','Skein-256(128)','Skein-512(128)');
-  }
-  if(preg_match('/^0x[a-f0-9]{32}$/i', $str)) {
-    array_push($possibleHashes,'Lineage II C4');
-  }
-  if(preg_match('/^[a-f0-9]{32}:[a-z0-9]{32}$/i', $str)) {
-    array_push($possibleHashes,'MD5(Joomla)');
-  }
-  if(preg_match('/^[a-f0-9]{32}:.{5}$/i', $str)) {
-    array_push($possibleHashes,'MD5(IP.Board)');
-  }
-  if(preg_match('/^[a-f-0-9]{32}:[a-z0-9]{8}$/i', $str)) {
-    array_push($possibleHashes,'MD5(MyBB)');
-  }
-  if(preg_match('/^[a-f0-9]{40}$/i', $str)) {
-    array_push($possibleHashes,'SHA-1','MySQL4.x','RIPEMD-160','Haval-160','SHA-1(MaNGOS)','SHA-1(MaNGOS2)','Tiger-160','Skein-256(160)','Skein-512(160)');
-  }
-  if(preg_match('/^\*[a-f0-9]{40}$/i', $str)) {
-    array_push($possibleHashes,'MySQL5.x');
-  }
-  if(preg_match('/^sha1\$.{0,32}\$[a-f0-9]{40}$/i', $str)) {
-    array_push($possibleHashes,'SHA-1(Django)');
-  }
-  if(preg_match('/^0x0100[a-f0-9]{0,8}?[a-f0-9]{40}$/i', $str)) {
-    array_push($possibleHashes,'MSSQL(2005)','MSSQL(2008)');
-  }
-  if(preg_match('/^[a-f0-9]{48}$/i', $str)) {
-    array_push($possibleHashes,'Haval-192','Tiger-192');
-  }
-  if(preg_match('/^[a-f0-9]{51}$/i', $str)) {
-    array_push($possibleHashes,'MD5(Palshop)');
-  }
-  if(preg_match('/^\$S\$[a-z0-9\/\.]{52}$/i', $str)) {
-    array_push($possibleHashes,'SHA-512(Drupal)');
-  }
-  if(preg_match('/^\$2a\$[0-9]{0,2}?\$[a-z0-9\/\.]{53}$/i', $str)) {
-    array_push($possibleHashes,'Blowfish(OpenBSD)');
-  }
-  if(preg_match('/^[a-f0-9]{56}$/i', $str)) {
-    array_push($possibleHashes,'SHA-224','Haval-224','Keccak-224','Skein-256(224)','Skein-512(224)');
-  }
-  if(preg_match('/^[a-f0-9]{64}$/i', $str)) {
-    array_push($possibleHashes,'SHA-256','RIPEMD-256','Haval-256','Snefru-256','GOST R 34.11-94','Keccak-256','Skein-256','Skein-512(256)');
-  }
-  if(preg_match('/^sha256\$.{0,32}\$[a-f0-9]{64}$/i', $str)) {
-    array_push($possibleHashes,'SHA-256(Django)');
-  }
-  if(preg_match('/^\$5\$.{0,22}\$[a-z0-9\/\.]{43,69}$/i', $str)) {
-    array_push($possibleHashes,'SHA-256(Unix)');
-  }
-  if(preg_match('/^[a-f0-9]{80}$/i', $str)) {
-    array_push($possibleHashes,'RIPEMD-320');
-  }
-  if(preg_match('/^0x0100[a-f0-9]{0,8}?[a-f0-9]{80}$/i', $str)) {
-    array_push($possibleHashes,'MSSQL(2000)');
-  }
-  if(preg_match('/^\$6\$.{0,22}\$[a-z0-9\/\.]{86}$/i', $str)) {
-    array_push($possibleHashes,'SHA-512(Unix)');
-  }
-  if(preg_match('/^[a-f0-9]{96}$/i', $str)) {
-    array_push($possibleHashes,'SHA-384','Keccak-384','Skein-512(384)','Skein-1024(384)');
-  }
-  if(preg_match('/^sha384\$.{0,32}\$[a-f0-9]{96}$/i', $str)) {
-    array_push($possibleHashes,'SHA-384(Django)');
-  }
-  if(preg_match('/^[a-f0-9]{128}$/i', $str)) {
-    array_push($possibleHashes,'SHA-512','Whirlpool','Keccak-512','Skein-512','Skein-1024(512)');
-  }
-  if(preg_match('/^[a-f0-9]{256}$/i', $str)) {
-    array_push($possibleHashes,'Skein-1024');
-  }
-	
+     
   //no hash found
-  if (empty($possibleHashes)) {
-    return $possibleHashes = array('Unknow Hash');
+  if(empty($possibleHashes)) {
+    return $possibleHashes = array('Unknown Hash');
   }
   //return the array
   else {
     return $possibleHashes;
   }
 }
-
+     
 //check if the form got submitted
 if(isset($_POST['submit']))
 {
   //save it in a variable
   $hash = $_POST['hash'];
-  
+     
   //trim possible whitespaces
   $hash = trim($hash);
-	
+           
   //check if empty form submitted
   if(empty($hash) && !is_numeric($hash)) {
     die('<script type="text/javascript">alert("no input detected!")</script>');
   }
-	
+           
   //save the array
   $hashes = IdentifyHash($hash);
-	
+     
   //show most and less possible results
   if(count($hashes) > 2) {
     //save most possible hashes
@@ -181,9 +131,10 @@ if(isset($_POST['submit']))
   //show absolut result
   else {
     //print the results to the screen
-    echo "<br />".implode("<br />",$hashes)."<br /><br />";
+     echo "<br />".implode("<br />",$hashes)."<br /><br />";
   }
 }
+
 ?>
 
 </body>
