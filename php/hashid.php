4fdd4ee9c1b2bbc24f887cf88e398edfa4e4be75
c0re <c0re@psypanda.org>
2014-03-06 14:45:13 +0100

    various new hashes added
    added:
    Crypt16
    BSDi Crypt
    PHPass' Portable Hash
    SCRAM Hash
    HAS-160
    Oracle 7-10g
    BCrypt(SHA256)
    
    updated regex:
    SHA1(Django)
    SHA-256(Django)
    SHA-384(Django)
    Minecraft(AuthMe Reloaded)
    GRUB2
    MD5(Unix)
    Cisco-IOS(MD5)
    FreeBSD MD5

--- php/hashid.php
+++ php/hashid.php
@@ -2,10 +2,10 @@
 /******************************************************************************
 * @name: hashid.php
 * @author: c0re <https://psypanda.org/>							
-* @date: 2013/03/05
+* @date: 2013/03/06
 * @copyright: <https://www.gnu.org/licenses/gpl-3.0.html>
 ******************************************************************************/
-$version = "v2.3.5";
+$version = "v2.3.6";
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -36,26 +36,28 @@ function IdentifyHash($str)
     '/^[a-f0-9]{8}$/i' => 'Adler-32,CRC-32,CRC-32B,FCS-32,GHash-32-3,GHash-32-5,FNV-132,Fletcher-32,Joaat,ELF-32,XOR-32',
     '/^\+[a-z0-9\/\.]{12}$/i' => 'Blowfish(Eggdrop)',
     '/^[a-z0-9\/\.]{13}$/i' => 'DES(Unix),Traditional DES,DEScrypt',
-    '/^[a-f0-9]{16}$/i' => 'MySQL3.x,DES(Oracle),VNC,Half MD5,FNV-164,CRC-64',
-    '/^[a-z0-9\/\.]{16}$/i' => 'Cisco-PIX MD5',
+    '/^[a-f0-9]{16}$/i' => 'MySQL323,DES(Oracle),VNC,Half MD5,FNV-164,CRC-64',
+    '/^[a-z0-9\/\.]{16}$/i' => 'Cisco-PIX(MD5)',
     '/^\\([a-z0-9\+\/]{20}\)$/i' => 'Lotus Domino',
+    '/^_[a-z0-9\/\.]{19}$/i' => 'BSDi Crypt',
     '/^[a-f0-9]{24}$/i' => 'CRC-96(ZIP)',
+    '/^[a-z0-9\/\.]{24}$/i' => 'Crypt16',
     '/^[0-9a-f]{32}$/i' => 'MD5,NTLM,LM,MD4,MD2,RAdmin v2.x,RIPEMD-128,Haval-128,Tiger-128,Snefru-128,MD5(ZipMonster),Skein-256(128),Skein-512(128)',
     '/^{SHA}[a-z0-9\/\+]{27}=$/i' => 'SHA-1(Base64),Netscape LDAP SHA',
-    '/^\$1\$.{0,8}\$[a-z0-9\/\.]{22}$/i' => 'MD5(Unix),Cisco-IOS MD5,FreeBSD MD5',
+    '/^\$1\$[a-z0-9\/\.]{0,8}\$[a-z0-9\/\.]{22}$/i' => 'MD5(Unix),Cisco-IOS(MD5),FreeBSD MD5',
     '/^0x[a-f0-9]{32}$/i' => 'Lineage II C4',
     '/^\$H\$[a-z0-9\/\.]{31}$/i' => 'MD5(phpBB3)',
-    '/^\$P\$[a-z0-9\/\.]{31}$/i' => 'MD5(Wordpress)',
+    '/^\$P\$[a-z0-9\/\.]{31}$/i' => 'MD5(Wordpress),PHPass\' Portable Hash',
     '/^[a-f0-9]{32}\:[a-z0-9]{2}$/i' => 'osCommerce,xt:Commerce',
     '/^\$apr1\$.{0,8}\$[a-z0-9\/\.]{22}$/i' => 'MD5(APR),Apache MD5',
     '/^{smd5}.{31}$/i' => 'AIX(smd5)',
     '/^[a-f0-9]{32}\:[0-9]{4}$/i' => 'WebEdition CMS',
     '/^[a-f0-9]{32}\:.{5}$/i' => 'IP.Board v2+,MyBB v1.2+',
     '/^[a-z0-9]{34}$/i' => 'CryptoCurrency(Adress)',
-    '/^[a-f0-9]{40}$/i' => 'SHA-1,MySQL4.x,RIPEMD-160,Haval-160,SHA-1(MaNGOS),SHA-1(MaNGOS2),Tiger-160,Skein-256(160),Skein-512(160)',
-    '/^\*[a-f0-9]{40}$/i' => 'MySQL5.x',
+    '/^[a-f0-9]{40}$/i' => 'SHA-1,RIPEMD-160,Haval-160,SHA-1(MaNGOS),SHA-1(MaNGOS2),Tiger-160,HAS-160,Skein-256(160),Skein-512(160)',
+    '/^\*[a-f0-9]{40}$/i' => 'MySQL5.x,MySQL4.1',
     '/^[a-z0-9]{43}$/i' => 'Cisco-IOS(SHA256)',
-    '/^[a-f-0-9]{32}\:.+$/i' => 'Domain Cached Credentials 2',
+    '/^[a-f-0-9]{32}:[^\\\/:*?\"\<\>\|]{1,15}$/i' => 'Domain Cached Credentials 2',
     '/^{SSHA}[a-z0-9\+\/]{38}={0,2}$/i' => 'SSHA-1(Base64),Netscape LDAP SSHA',
     '/^[a-z0-9]{47}$/i' => 'FortiOS',
     '/^[a-f0-9]{48}$/i' => 'Haval-192,Tiger-192,OSX v10.4,OSX v10.5,OSX v10.6',
@@ -67,6 +69,7 @@ function IdentifyHash($str)
     '/^[a-f0-9]{56}$/i' => 'SHA-224,Haval-224,Keccak-224,Skein-256(224),Skein-512(224)',
     '/^(\$2a|\$2y|\$2)\$[0-9]{0,2}?\$[a-z0-9\/\.]{53}$/i' => 'Blowfish(OpenBSD)',
     '/^S\:[a-f0-9]{60}$/i' => 'Oracle 11g',
+    '/^\$bcrypt-sha256\$.{5}\$[a-z0-9\/\.]{22}\$[a-z0-9\/\.]{31}$/i' => 'BCrypt(SHA256)',
     '/^[a-f0-9]{32}\:[a-z0-9]{30}$/i' => 'vBulletin >v3.8.5',
     '/^[a-f0-9]{64}$/i' => 'SHA-256,RIPEMD-256,Haval-256,Snefru-256,GOST R 34.11-94,Keccak-256,Skein-256,Skein-512(256)',
     '/^[a-f0-9]{32}\:[a-z0-9]{32}$/i' => 'Joomla',
@@ -84,15 +87,16 @@ function IdentifyHash($str)
     '/^0x0200[a-f0-9]{136}$/i' => 'MSSQL(2012)',
     '/^\$ml\$.+$/i' => 'OSX v10.8',
     '/^[a-f0-9]{256}$/i' => 'Skein-1024',
-    '/^grub\.pbkdf2.+$/i' => 'GRUB 2',
-    '/^sha1\$.{0,32}\$[a-f0-9]{40}$/i' => 'SHA-1(Django)',
+    '/^grub\.pbkdf2\.sha512\..+$/i' => 'GRUB 2',
+    '/^sha1\$[a-z0-9\/\.]{1,12}\$[a-f0-9]{40}$/i' => 'SHA-1(Django)',
     '/^\$S\$[a-z0-9\/\.]{52}$/i' => 'SHA-512(Drupal)',
     '/^\$5\$.{0,22}\$[a-z0-9\/\.]{43,69}$/i' => 'SHA-256(Unix)',
     '/^\$6\$.{0,22}\$[a-z0-9\/\.]{86}$/i' => 'SHA-512(Unix)',
-    '/^\$sha\$[a-z0-9]{0,16}\$[a-f0-9]{64}$/i' => 'Minecraft(AuthMe Reloaded)',
-    '/^sha256\$.{0,32}\$[a-f0-9]{64}$/i' => 'SHA-256(Django)',
-    '/^sha384\$.{0,32}\$[a-f0-9]{96}$/i' => 'SHA-384(Django)',
-    '/^[^\\/\:\*\?\"\<\>\|]{1,15}\:[^\\/\:\*\?\"\<\>\|]{1,15}\:[a-f0-9]{32}\:[a-f0-9]{32}\:{0,3}$/i' => 'Domain Cached Credentials'
+    '/^\$sha\$[a-z0-9]{1,16}\$[a-f0-9]{64}$/i' => 'Minecraft(AuthMe Reloaded)',
+    '/^sha256\$[a-z0-9\/\.]{1,12}\$[a-f0-9]{64}$/i' => 'SHA-256(Django)',
+    '/^sha384\$[a-z0-9\/\.]{1,12}\$[a-f0-9]{96}$/i' => 'SHA-384(Django)',
+    '/^[^\\/\:\*\?\"\<\>\|]{1,15}\:[^\\/\:\*\?\"\<\>\|]{1,15}\:[a-f0-9]{32}\:[a-f0-9]{32}\:{0,3}$/i' => 'Domain Cached Credentials',
+    '/^\$scram\$.+$/i' => 'SCRAM Hash'
   );
            
   //initialize the array
--- python/hashid.py
+++ python/hashid.py
@@ -2,14 +2,13 @@
 # -*- coding: utf-8 -*-
 # @name: hashID.py
 # @author: c0re <https://psypanda.org/>                           
-# @date: 2014/03/05
+# @date: 2014/03/06
 # @copyright: <https://www.gnu.org/licenses/gpl-3.0.html>
-
 
 import re, os, argparse
 
 #set the version
-version = "v2.2.2"
+version = "v2.2.3"
 #set the banner
 banner = "%(prog)s " + version + " by c0re <https://github.com/psypanda/hashID>\nLicense GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>"
 #set the usage
@@ -43,26 +42,28 @@ def identifyHash(hash):
     ("^[a-f0-9]{8}$", ("Adler-32","CRC-32","CRC-32B","FCS-32","GHash-32-3","GHash-32-5","FNV-132","Fletcher-32","Joaat","ELF-32","XOR-32")),
     ("^\+[a-z0-9\/\.]{12}$", ("Blowfish(Eggdrop)",)),
     ("^[a-z0-9\/\.]{13}$", ("DES(Unix)","Traditional DES","DEScrypt")),
-    ("^[a-f0-9]{16}$", ("MySQL3.x","DES(Oracle)","VNC","Half MD5","FNV-164","CRC-64")),
-    ("^[a-z0-9\/\.]{16}$", ("Cisco-PIX MD5",)),
+    ("^[a-f0-9]{16}$", ("MySQL323","DES(Oracle)","VNC","Half MD5","Oracle 7-10g","FNV-164","CRC-64")),
+    ("^[a-z0-9\/\.]{16}$", ("Cisco-PIX(MD5)",)),
     ("^\([a-z0-9\+\/]{20}\)$", ("Lotus Domino",)),
+    ("^_[a-z0-9\/\.]{19}$", ("BSDi Crypt",)),
     ("^[a-f0-9]{24}$", ("CRC-96(ZIP)",)),
+    ("^[a-z0-9\/\.]{24}$", ("Crypt16",)),
     ("^[0-9a-f]{32}$", ("MD5","NTLM","LM","MD4","MD2","RAdmin v2.x","RIPEMD-128","Haval-128","Tiger-128","Snefru-128","MD5(ZipMonster)","Skein-256(128)","Skein-512(128)")),
     ("^{SHA}[a-z0-9\/\+]{27}=$", ("SHA-1(Base64)","Netscape LDAP SHA")),
-    ("^\$1\$.{0,8}\$[a-z0-9\/\.]{22}$", ("MD5(Unix)","Cisco-IOS MD5","FreeBSD MD5")),
+    ("^\$1\$[a-z0-9\/\.]{0,8}\$[a-z0-9\/\.]{22}$", ("MD5(Unix)","Cisco-IOS(MD5)","FreeBSD MD5")),
     ("^0x[a-f0-9]{32}$", ("Lineage II C4",)), 
     ("^\$H\$[a-z0-9\/\.]{31}$", ("MD5(phpBB3)",)),
-    ("^\$P\$[a-z0-9\/\.]{31}$", ("MD5(Wordpress)",)),
+    ("^\$P\$[a-z0-9\/\.]{31}$", ("MD5(Wordpress)","PHPass' Portable Hash")),
     ("^[a-f0-9]{32}:[a-z0-9]{2}$", ("osCommerce","xt:Commerce")),
     ("^\$apr1\$.{0,8}\$[a-z0-9\/\.]{22}$", ("MD5(APR)","Apache MD5")),
     ("^{smd5}.{31}$", ("AIX(smd5)",)),
     ("^[a-f0-9]{32}:[0-9]{4}$", ("WebEdition CMS",)),
     ("^[a-f0-9]{32}:.{5}$", ("IP.Board v2+","MyBB v1.2+")),
     ("^[a-z0-9]{34}$", ("CryptoCurrency(Adress)",)),
-    ("^[a-f0-9]{40}$", ("SHA-1","MySQL4.x","RIPEMD-160","Haval-160","SHA-1(MaNGOS)","SHA-1(MaNGOS2)","Tiger-160","Skein-256(160)","Skein-512(160)")),
-    ("^\*[a-f0-9]{40}$", ("MySQL5.x",)),
+    ("^[a-f0-9]{40}$", ("SHA-1","RIPEMD-160","Haval-160","SHA-1(MaNGOS)","SHA-1(MaNGOS2)","Tiger-160","HAS-160","Skein-256(160)","Skein-512(160)")),
+    ("^\*[a-f0-9]{40}$", ("MySQL5.x","MySQL4.1")),
     ("^[a-z0-9]{43}$", ("Cisco-IOS(SHA256)",)),
-    ("^[a-f-0-9]{32}:.+$", ("Domain Cached Credentials 2",)),
+    ("^[a-f-0-9]{32}:[^\\\/:*?\"\<\>\|]{1,15}$", ("Domain Cached Credentials 2",)),
     ("^{SSHA}[a-z0-9\+\/]{38}={0,2}$", ("SSHA-1(Base64)","Netscape LDAP SSHA")),
     ("^[a-z0-9]{47}$", ("FortiOS",)),
     ("^[a-f0-9]{48}$", ("Haval-192","Tiger-192","OSX v10.4","OSX v10.5","OSX v10.6")),
@@ -74,6 +75,7 @@ def identifyHash(hash):
     ("^[a-f0-9]{56}$", ("SHA-224","Haval-224","Keccak-224","Skein-256(224)","Skein-512(224)")),
     ("^(\$2a|\$2y|\$2)\$[0-9]{0,2}?\$[a-z0-9\/\.]{53}$", ("Blowfish(OpenBSD)",)),
     ("^S:[a-f0-9]{60}$", ("Oracle 11g",)),
+    ("^\$bcrypt-sha256\$.{5}\$[a-z0-9\/\.]{22}\$[a-z0-9\/\.]{31}$", ("BCrypt(SHA256)",)),
     ("^[a-f0-9]{32}:[a-z0-9]{30}$", ("vBulletin >v3.8.5",)),
     ("^[a-f0-9]{64}$", ("SHA-256","RIPEMD-256","Haval-256","Snefru-256","GOST R 34.11-94","Keccak-256","Skein-256","Skein-512(256)")),
     ("^[a-f0-9]{32}:[a-z0-9]{32}$", ("Joomla",)),
@@ -91,15 +93,16 @@ def identifyHash(hash):
     ("^0x0200[a-f0-9]{136}$", ("MSSQL(2012)",)),
     ("^\$ml\$.+$", ("OSX v10.8",)),
     ("^[a-f0-9]{256}$", ("Skein-1024",)),
-    ("^grub\.pbkdf2.+$", ("GRUB 2",)),
-    ("^sha1\$.{0,32}\$[a-f0-9]{40}$", ("SHA-1(Django)",)),
+    ("^grub\.pbkdf2\.sha512\..+$", ("GRUB 2",)),
+    ("^sha1\$[a-z0-9\/\.]{1,12}\$[a-f0-9]{40}$", ("SHA-1(Django)",)),
     ("^\$S\$[a-z0-9\/\.]{52}$", ("SHA-512(Drupal)",)),
     ("^\$5\$.{0,22}\$[a-z0-9\/\.]{43,69}$", ("SHA-256(Unix)",)),
     ("^\$6\$.{0,22}\$[a-z0-9\/\.]{86}$", ("SHA-512(Unix)",)),
-    ("^\$sha\$[a-z0-9]{0,16}\$[a-f0-9]{64}$", ("Minecraft(AuthMe Reloaded)",)),
-    ("^sha256\$.{0,32}\$[a-f0-9]{64}$", ("SHA-256(Django)",)),
-    ("^sha384\$.{0,32}\$[a-f0-9]{96}$", ("SHA-384(Django)",)),
-    ("^[^\\\/:*?\"\<\>\|]{1,15}:[^\\\/:*?\"\<\>\|]{1,15}:[a-f0-9]{32}:[a-f0-9]{32}:{0,3}$", ("Domain Cached Credentials",))
+    ("^\$sha\$[a-z0-9]{1,16}\$[a-f0-9]{64}$", ("Minecraft(AuthMe Reloaded)",)),
+    ("^sha256\$[a-z0-9\/\.]{1,12}\$[a-f0-9]{64}$", ("SHA-256(Django)",)),
+    ("^sha384\$[a-z0-9\/\.]{1,12}\$[a-f0-9]{96}$", ("SHA-384(Django)",)),
+    ("^[^\\\/:*?\"\<\>\|]{1,15}:[^\\\/:*?\"\<\>\|]{1,15}:[a-f0-9]{32}:[a-f0-9]{32}:{0,3}$", ("Domain Cached Credentials",)),
+    ("^\$scram\$.+$", ("SCRAM Hash",))
   )
   for hashtype in prototypes:
     #try to find matches
@@ -139,6 +142,8 @@ def showResult(list):
 #analyse and write file
 def analyseFile(infile,outfile):
   hashesAnalysed = 0
+  #show input file path
+  print ("Analysing '" + os.path.abspath(infile.name) + "'")
   for line in infile:
     #increment hash count
     hashesAnalysed += 1
@@ -151,7 +156,7 @@ def analyseFile(infile,outfile):
   #show number of hashes found
   print ("Hashes found: " + str(foundHashes))
   #show output file path
-  print ("Output written: " + os.path.abspath(outfile.name))
+  print ("Output written: '" + os.path.abspath(outfile.name) + "'")
 
 
 #analyse a single hash
