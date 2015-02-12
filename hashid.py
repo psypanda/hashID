ea48085619685d3a6a2eade5441dbf3eacbc393a
c0re <c0re@psypanda.org>
2015-02-12 15:16:02 +0100

    Add JtR format for BigCrypt

--- hashid.py
+++ hashid.py
@@ -692,7 +692,7 @@ prototypes = [
     Prototype(
         regex=re.compile(r'^[a-z0-9\/.]{13,}$', re.IGNORECASE),
         modes=[
-            HashInfo(name='BigCrypt', hashcat=None, john=None, extended=True)]),
+            HashInfo(name='BigCrypt', hashcat=None, john='bigcrypt', extended=True)]),
     Prototype(
         regex=re.compile(r'^(\$cisco4\$)?[a-z0-9\/.]{43}$', re.IGNORECASE),
         modes=[
