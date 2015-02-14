3319f3caf18029c0a273c97383cf226bb471f89a
c0re <c0re@psypanda.org>
2015-02-14 16:28:09 +0100

    Increase version number
    Updated to v3.1.2

--- hashid.py
+++ hashid.py
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __author__  = "c0re"
-__version__ = "3.1.1"
+__version__ = "3.1.2"
 __github__  = "https://github.com/psypanda/hashID"
 __license__ = "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>"
 
