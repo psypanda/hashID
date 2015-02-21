912c81e081b0aead2285ec1e6115bf5759a08b96
c0re <c0re@psypanda.org>
2015-02-21 10:32:37 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+import io
+import os
+import re
+from setuptools import setup
 
-def get_version():
-    with open('hashid.py') as f:
-        for line in f:
-            if line.startswith('__version__'):
-                return eval(line.split('=')[-1])
 
-def get_long_description():
-    descr = []
-    for fname in 'README.rst', 'doc/CHANGELOG':
-        with open(fname) as f:
-            descr.append(f.read())
-    return '\n\n'.join(descr)
+def read(*parts):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with io.open(os.path.join(here, *parts), "r", encoding="utf-8") as f:
+        return f.read()
+
+
+def get_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
-    name = 'hashID',
-    packages = ['hashID'],
-    version = get_version(),
-    description = 'Software to identify the different types of hashes',
-    long_description = get_long_description(),
-    author = 'c0re',
-    author_email = 'c0re@psypanda.org',
-    license = 'GNU GPL',
-    url = 'https://github.com/psypanda/hashID',
-    download_url = 'https://github.com/psypanda/hashID/tarball/v' + get_version(),
-    keywords = ['hashid', 'hash', 'identifier', 'hash-identifier'],
-    classifiers = [
+    name='hashID',
+    packages=['hashID'],
+    version=get_version('hashid.py'),
+    description='Software to identify the different types of hashes',
+    long_description=read('README.rst'),
+    author='c0re',
+    author_email='c0re@psypanda.org',
+    license='GNU GPL',
+    url='https://github.com/psypanda/hashID',
+    download_url=url + '/tarball/v' + get_version('hashid.py'),
+    keywords='hashid hash identifier hash-identifier',
+    py_modules=['hashid'],
+    # packages=find_packages(exclude=['man', 'hashinfo.xlsx'])
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
+        'Natural Language :: English',
         'Intended Audience :: Other Audience',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
     ],
 )
